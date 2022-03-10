75a69493015f3f265cce13fe8b67e6bec40b63b9
Niros <niro655@gmail.com>
2022-03-11 00:45:49 +0200

    Moralis integration

--- src/components/components/wallet.js
+++ src/components/components/wallet.js
@@ -1,74 +1,100 @@
 import React from 'react';
+import {useMoralis} from 'react-moralis';
 
+const Wallet= () => {
+    const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
 
-const Wallet= () => (
-  <div className="row">
-    <div className="col-lg-3 mb30">
+    const login = async () => {
+        if (!isAuthenticated) {
+            await authenticate({signingMessage: "Log in using Moralis" })
+                .then((user) =>  {
+                    console.log("logged in user:", user);
+                    console.log(user?.get("ethAddress"));
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    }
+
+    const logOut = async () => {
+        await logout();
+        console.log("logged out");
+    }
+
+    return (
+        <div>
+          {isAuthenticated && <div>Logged in as: {account}</div>}
+          {isAuthenticated && <button onClick={logOut} disabled={isAuthenticating}>Logout</button>}
+          <br />
+            <div className="row">
+                <div className="col-lg-3 mb30" onClick={login}>
         <span className="box-url">
             <span className="box-url-label">Most Popular</span>
             <img src="./img/wallet/1.png" alt="" className="mb20"/>
             <h4>Metamask</h4>
             <p>Start exploring blockchain applications in seconds.  Trusted by over 1 million users worldwide.</p>
         </span>
-    </div>
-
-    <div className="col-lg-3 mb30">
+                </div>
+                <div className="col-lg-3 mb30">
         <span className="box-url">
             <img src="./img/wallet/2.png" alt="" className="mb20"/>
             <h4>Bitski</h4>
             <p>Bitski connects communities, creators and brands through unique, ownable digital content.</p>
         </span>
-    </div>       
+                </div>
 
-    <div className="col-lg-3 mb30">
+                <div className="col-lg-3 mb30">
         <span className="box-url">
             <img src="./img/wallet/3.png" alt="" className="mb20"/>
             <h4>Fortmatic</h4>
             <p>Let users access your Ethereum app from anywhere. No more browser extensions.</p>
         </span>
-    </div>    
+                </div>
 
-    <div className="col-lg-3 mb30">
+                <div className="col-lg-3 mb30">
         <span className="box-url">
             <img src="./img/wallet/4.png" alt="" className="mb20"/>
             <h4>WalletConnect</h4>
             <p>Open source protocol for connecting decentralised applications to mobile wallets.</p>
         </span>
-    </div>
+                </div>
 
-    <div className="col-lg-3 mb30">
+                <div className="col-lg-3 mb30">
         <span className="box-url">
             <img src="./img/wallet/5.png" alt="" className="mb20"/>
             <h4>Coinbase Wallet</h4>
             <p>The easiest and most secure crypto wallet. ... No Coinbase account required.
             </p>
         </span>
-    </div>
+                </div>
 
-    <div className="col-lg-3 mb30">
+                <div className="col-lg-3 mb30">
         <span className="box-url">
             <img src="./img/wallet/6.png" alt="" className="mb20"/>
             <h4>Arkane</h4>
             <p>Make it easy to create blockchain applications with secure wallets solutions.</p>
         </span>
-    </div>       
+                </div>
 
-    <div className="col-lg-3 mb30">
+                <div className="col-lg-3 mb30">
         <span className="box-url">
             <img src="./img/wallet/7.png" alt="" className="mb20"/>
             <h4>Authereum</h4>
             <p>Your wallet where you want it. Log into your favorite dapps with Authereum.</p>
         </span>
-    </div>    
+                </div>
 
-    <div className="col-lg-3 mb30">
+                <div className="col-lg-3 mb30">
         <span className="box-url">
             <span className="box-url-label">Most Simple</span>
             <img src="./img/wallet/8.png" alt="" className="mb20"/>
             <h4>Torus</h4>
             <p>Open source protocol for connecting decentralised applications to mobile wallets.</p>
         </span>
-    </div>                                  
-</div>
-);
+                </div>
+            </div>
+        </div>
+    )
+};
 export default Wallet;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {MoralisProvider} from 'react-moralis';
 import "./assets/animated.css";
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/elegant-icons/style.css';
 import '../node_modules/et-line/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -11,7 +12,9 @@ import App from './components/app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-	<App />, document.getElementById('root'));
+		<MoralisProvider serverUrl="https://ktx9slty1spy.usemoralis.com:2053/server" appId="qB8zVHSILn5P7HXNkkzplE3RIEmfpE9giGGT3WKP">
+			<App />
+		</MoralisProvider>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
